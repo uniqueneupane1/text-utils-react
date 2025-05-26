6ad22c00159f60c0084525b59452682f4c65104e
Unique Neupane <uniqueneupane935@gmail.com>
2025-05-26 07:27:45 +0545

    Enhance TextForm: word count, read time,  and disable button when empty

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
     // console.log("Props received", props);
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             {props.title}
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,32 +1,53 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 export default function TextForm(props) {
+  const [text, setText] = useState("");
+
+
   const handleUpClick = () => {
     // console.log("UpperCase was clicked" + text);
     let newText = text.toUpperCase();
-    setText(newText); 
+    setText(newText);
+  };
+
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
   }
-  
+
   const handleOnChange = (event) => {
     // console.log("Value Changed");
     setText(event.target.value);
-  }
-  const [text, setText] = useState('Enter text here');
-  return (
+  };
 
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className ="form-control"
-          id="myBox"
-          value={text}
-          onChange={handleOnChange}
-          rows="8"
-        ></textarea>
+  const isTextEmpty = text.trim() === "";
+  return (
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="myBox"
+            placeholder="Enter text here"
+            onChange={handleOnChange}
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-outline-success" onClick={handleUpClick} disabled = {isTextEmpty}>
+          Convert to upper case
+        </button>
+        <button className="btn btn-outline-success mx-2" onClick={handleLoClick} disabled = {isTextEmpty}>
+          Convert to lower case
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert to upper case</button>
-    </div>
+      <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p><b>{isTextEmpty ? 0 : text.trim().split(/\s+/).length} words and {text.length} characters</b></p>
+        <p><b>{0.008 * (isTextEmpty ? 0 : text.trim().split(/\s+/).length)} minutes to read</b></p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
