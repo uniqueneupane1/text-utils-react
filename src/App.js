b4e3de25f066a41ff0f954549241e3ea65f05911
Unique Neupane <uniqueneupane935@gmail.com>
2025-05-27 07:43:59 +0545

    Add 'React Router' setup with routes for Home and About pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Navbar title="TextUtils" aboutText="About Us" />
+    <Router>
+      <Navbar title="TextUtils" aboutText="About" />
       {/* <Navbar /> */}
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze"/>
+        <Routes>
+          <Route
+            path="/"
+            element={<TextForm heading="Enter the text to analyze" />}
+          />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 export default App;
